91953f7ae0b4b438c3ab05a87a96d1369a7bf374
Nils Laurent <nils.laurent1@univ-grenoble-alpes.fr>
2022-03-02 15:58:58 +0100

    add snr function

---
+++ TF_Toolbox/snr.m
@@ -0,0 +1,21 @@
+function [snr_out] = snr(s,n)
+%SNR signal to noise ratio
+%   [snr_out] = SNR(s, n)
+%   s        : signal
+%   n        : noise
+%
+%   --  Output
+%   snr_out  : output SNR
+
+if length(size(s)) == 2 && length(size(n)) == 2
+    if min(size(s)) == 1 && min(size(n)) == 1
+        s = s(:);
+        n = n(:);
+    end
+end
+
+rms = @(x) sqrt(mean(abs(x).^2));
+snr_out = 20 * log10(rms(s)/rms(n));
+
+end
+
