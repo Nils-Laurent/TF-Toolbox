7ced149fea4670705f253dac575058c5c0572f98
Nils Laurent <nils.laurent1@univ-grenoble-alpes.fr>
2022-04-19 11:44:22 +0200

    edit add_noise interface (real, imag)

--- TF_Toolbox/add_noise.m
+++ TF_Toolbox/add_noise.m
@@ -1,18 +1,58 @@
-function [y] = add_noise(x, n, SNR_in)
+function [y] = add_noise(x, SNR_in, varargin)
 %ADD_NOISE add noise to x such that the input SNR is SNR_in
-%   [y] = add_noise(x, n, SNR_in)
+%   [y] = add_noise(x, SNR_in)
+%   [y] = add_noise(x, SNR_in, n)
 %
 % INPUTS:
 %   x        : signal
-%   n        : noise
 %   SNR_in   : input SNR
+%
+%   n        : noise
+%              default is a real randn if x is real
+%                         or if parameter 'real' is true.
+%                      is a complex randn if x has non-zero
+%                         imaginary part or if parameter 'real' is true.
+%
+%   --  List of possible name-value pair argument
+%   'real'   : no effect if n is given,
+%              noise is real gaussian noise (see randn).
+%   'imag'   : no effect if n is given,
+%              noise is gaussian and has a nonzero imaginary part.
 %
 % OUTPUTS:
 %   --  Output
 %   y        : Noisy signal.
 
+L = length(x);
+defaultN = [];
+
+p = inputParser;
+addRequired(p, 'x');
+addRequired(p, 'SNR_in');
+addOptional(p, 'n', defaultN);
+addParameter(p, 'real', false);
+addParameter(p, 'imag', false);
+
+parse(p, x, SNR_in, varargin{:});
+r = p.Results;
+
+if (r.imag == true && r.real == true)
+  error("the noise connot be real and have nonzero imaginary part.");
+end
+
+if (sum(size(r.n)) > 0)
+    % r.n = r.n
+elseif (r.imag == true)
+  r.n = randn(1, L) + 1i*randn(1, L);
+elseif (r.real == true) || (sum(abs(imag(x))) > 0)
+  r.n = randn(1, L);
+else
+  r.n = randn(1, L) + 1i*randn(1, L);
+end
+
+
 x = x(:);
-n = n(:);
+r.n = r.n(:);
 
 if SNR_in == inf
     y = x(:);
@@ -20,11 +60,11 @@ if SNR_in == inf
 end
 
 if SNR_in == -inf
-    y = n(:);
+    y = r.n(:);
     return;
 end
 
-Scale = sqrt(sum(abs(x).^2)/sum(abs(n).^2)*10^(-SNR_in/10));
-y = x + Scale*n;
+Scale = sqrt(sum(abs(x).^2)/sum(abs(r.n).^2)*10^(-SNR_in/10));
+y = x + Scale*r.n;
 end
 
--- Validation/SV_addNoise.m
+++ Validation/SV_addNoise.m
@@ -11,7 +11,27 @@ x = ones(Lx, 1);
 for snr_in = SNRs
     for m=1:M
         n = randn(1, Lx);
-        y = add_noise(x, n, snr_in);
+        y = add_noise(x, snr_in, n);
+        snr_out = snr(x, y - x);
+        if abs(snr_in - snr_out) > th
+            error("snr diff too big\n");
+        end
+    end
+end
+
+for snr_in = SNRs
+    for m=1:M
+        y = add_noise(x, snr_in, 'real', true);
+        snr_out = snr(x, y - x);
+        if abs(snr_in - snr_out) > th
+            error("snr diff too big\n");
+        end
+    end
+end
+
+for snr_in = SNRs
+    for m=1:M
+        y = add_noise(x, snr_in, 'imag', true);
         snr_out = snr(x, y - x);
         if abs(snr_in - snr_out) > th
             error("snr diff too big\n");
